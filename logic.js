184fd66bce0239404c0154dcf90367a4f54899a9
AidenRIreland <aidenireland2004@gmail.com>
2024-01-30 02:50:04 -0500

    fixed links

--- logic.js
+++ logic.js
@@ -1,57 +1,323 @@
-/*import { 
-    Goku_Transformations,
-    Vegeta_Transformations, 
-    ADGohan_Transformations,
-    TeenGohan_Transformations,
-    Goten_Transformations,
-    Gogeta_Transformations,
-    Bardock_Transformations,
-    SBroly_Transformations,
-    KidTrunks_Transformations,
-    ADTrunks_Transformations,
-    Beerus,Whis,Piccolo,
-    Freiza,Cell,Buu,Pan,An16,An17,An18,
-    Ginyu,Videl,Krillin,Tien,Yamcha
-} from './transformations.js';*/
-/*function get_random_moves() {
-    const characters = [
-      { name: 'Goku', transformations: Goku_Transformations },
-      { name: 'Vegeta', transformations: Vegeta_Transformations },
-      { name: 'Adult Gohan',transformations: ADGohan_Transformations},
-      { name: 'Teen Gohan',transformations: TeenGohan_Transformations},
-      { name: 'Goten',transformations: Goten_Transformations},
-      { name: 'Gogeta',transformations: Gogeta_Transformations},
-      { name: 'Bardock',transformations: Bardock_Transformations},
-      { name: 'SBroly',transformations: SBroly_Transformations},
-      { name: 'Kid Trunks',transformations: KidTrunks_Transformations},
-      { name: 'Adult Trunks',transformations: ADTrunks_Transformations},
-      { name: 'Beerus',transformations: Beerus},
-      { name: 'Whis',transformations: Whis},
-      { name: 'Freiza',transformations: Freiza},
-      { name: 'Cell',transformations: Cell},
-      { name: 'Buu',transformations: Buu},
-      { name: 'Pan',transformations: Pan},
-      { name: 'Andriod 16',transformations: An16},
-      { name: 'Andriod 17',transformations: An17},
-      { name: 'Andriod 18',transformations: An18},
-      { name: 'Piccolo',transformations: Piccolo},
-      { name: 'Ginyu',transformations: Ginyu},
-      { name: 'Videl',transformations: Videl},
-      { name: 'Krillin',transformations: Krillin},
-      { name: 'Tien',transformations: Tien},
-      { name: 'Yamcha',transformations: Yamcha}
-    ];
-  
-    // Randomly select a character and their transformation
-    const characterIndex = Math.floor(Math.random() * characters.length);
-    const character = characters[characterIndex];
-    const forms = Object.keys(character.transformations);
-    const form = forms[Math.floor(Math.random() * forms.length)];
-    const moves = character.transformations[form];
+const Goku = {
+    "Normal": [
+        "Kamehameha",
+        "Kaioken Kamehameha",
+        "Kaioken Assault",
+        "Spirit Bomb",
+        "Super Spirit Bomb"
+    ],
+    "Super Saiyan": [
+        "Kamehameha",
+        "Super Kamehameha",
+        "Warp Kamehameha"
+    ],
+    "Super Saiyan 2": [
+        "Kamehameha",
+        "Super Kamehameha"
+    ],
+    "Super Saiyan God":[
+        "Kamehameha",
+        "Super Kamehameha",
+        "God Kamehameha"
+    ],
+    "Super Saiyan Blue":[
+        "Kamehameha",
+        "Super Kamehameha",
+        "10X Kaioken God Kamehameha"
+    ],
+}
+const Vegeta = {
+    "Normal": [
+        "Galick Gun",
+        "Full Power Energy Wave"
+    ],
+    "Super Saiyan": [
+        "Galick Gun",
+        "Big Bang Attack",
+        "Final Flash"
+    ],
+    "Super Saiyan 2": [
+        "Galick Gun",
+        "Full Power Energy Wave",
+        "Final Flash"
+    ],
+    "Super Saiyan God":[
+        "Galick Gun",
+        "Full Power Energy Wave",
+        "Big Bang Attack",
+        "Blazing Attack"
+    ],
+    "Super Saiyan Blue":[
+        "Galick Gun",
+        "Super Galick Gun",
+        "Big Bang Attack",
+        "Final Flash"
+    ],
+}
+const ADGohan = {
+    "Normal": [
+        "Kamehameha",
+        "Masenko",
+        "Full Power Energy Wave"
+    ],
+    "Super Sayian": [
+        "Kamehameha",
+        "Super Kamehameha",
+        "Masenko"
+    ],
+    "Great Saiyaman 1":[
+        "Kamehameha",
+        "Masenko",
+        "Full Power Energy Wave"
+    ],
+    "Beast":[
+        "Kamehameha",
+        "Masenko",
+        "Demon Ray",
+        "Special Beam Cannon (Beast)"
+    ]
+}
+const TeenGohan ={
+    "Normal": [
+        "Kamehameha",
+        "Masenko",
+        "Full Power Energy Wave"
+    ],
+    "Super Saiyan": [
+        "Kamehameha",
+        "Masenko",
+        "Full Power Energy Wave"
+    ],
+    "Super Saiyan": [
+        "Kamehameha",
+        "Father Son Kamehameha",
+        "Full Power Energy Wave",
+        "Motionless Kamehameha"
+    ],
+}
+const Goten = {
+    "Normal": [
+        "Kamehameha",
+        "Charge",
+        "Full Power Energy Blast Volley"
+    ],
+    "Super Saiyan": [
+        "Kamehameha",
+        "Super Kamehameha",
+        "Charge",
+        "Full Power Energy Blast Volley"
+    ],
+}
+const Gogeta = {
+    "Normal": [
+        "Kamehameha",
+        "Spirit Blaster"
+    ],
+    "Super Saiyan Blue": [
+        "Super Kamehameha",
+        "Soul Punisher",
+        "God Punisher"
+    ],
+}
+const Bardock = {
+    "Normal": [
+        "Full Power Energy Wave",
+        "Rebellion Spear"
+    ],
+    "Super Saiyan": [
+        "Riot Javelin",
+        "Full Power Energy Wave",
+        "Rebellion Spear"
+    ],
+}
+const SBroly = {
+    "Normal": [
+        "Powered Shell",
+        "Gigantic Charge"
+    ],
+    "Super Saiyan": [
+        "Powered Shell",
+        "Gigantic Charge",
+        "Gigantic Meteor"
+    ],
+}
+const KidTrunks ={
+    "Normal": [
+        "Full Power Energy Wave",
+        "Full Power Energy Blast Volley",
+        "Double Buster"
+    ],
+    "Super Saiyan": [
+        "Full Power Energy Wave",
+        "Full Power Energy Blast Volley",
+        "Double Buster",
+        "Finish Buster"
+    ],
+}
+const ADTrunks ={
+    "Normal": [
+        "Full Power Energy Wave",
+        "Full Power Energy Blast Volley",
+        "Burning Attack"
+    ],
+    "Super Saiyan": [
+        "Full Power Energy Blast Volley",
+        "Burning Slash"
+    ],
+}
+const Beerus = {
+    "Normal": [
+        "Full Power Energy Wave",
+        "Sphere of Destruction"
+    ]
+}
+const Whis = {
+    "Normal": [
+        "Prelude to Destruction",
+        "Symphonic Destruction"
+    ]
+}
+const Piccolo = {
+    "Normal": [
+        "Evil Explosion",
+        "Special Beam Cannon",
+        "Hellzone Grenade",
+        "Light Grenade"
+    ],
+    "Orange": [
+        "Full Power Energy Wave",
+        "Special Beam Cannon",
+        "Apocalyptic Burst",
+        "Fierce Fist"
+    ]
+}
+const Frieza = {
+    "4th Form": [
+        "Death Slicer",
+        "Death Ball"
+    ],
+    "Golden":[
+        "Crazy Finger Shot",
+        "Emperor's Death Beam",
+        "Death Slicer"
+    ]
+}
+const Cell = {
+    "Perfect Form":[
+        "Full Power Energy Blast Volley",
+        "Special Beam Cannon",
+        "Kamehameha",
+        "Perfect Kamehameha"
+    ]
+}
+const Buu = {
+    "Innocent":[
+        "Full Power Energy Wave",
+        "Innocence Cannon"
+    ],
+    "Kid":[
+        "Full Power Energy Wave",
+        "Majin Kamehameha",
+        "Vanishing Ball"
+    ]
+
+}
+const GT_Pan = {
+    "Normal": [
+        "Maiden Blast",
+        "Full Power Energy Wave",
+        "Kamehameha",
+    ],
+}
+const An16 = {
+    "Normal": [
+        "Rocket Punch",
+        "Hell Flash",
+    ],
+}
+const An17 = {
+    "Normal": [
+        "Side Bridge",
+        "Full Power Energy Blast Volley",
+        "Super Electric Strike",
+    ],
+}
+const An18 = {
+    "Normal": [
+        "Destructo-Disc",
+        "Full Power Energy Blast Volley",
+        "Dual Destructo-Disc",
+    ],
+}
+const Ginyu = {
+    "Normal": [
+        "Full Power Energy Blast Volley",
+        "Full Power Energy Wave",
+        "Milky Cannon",
+    ],
+}
+const Videl ={
+    "Normal": [
+        "Full Power Energy Blast Volley",
+        "Full Power Energy Wave",
+        "Justice Rush",
+    ],
+    "Great Saaiyaman 2": [
+        "Full Power Energy Blast Volley",
+        "Full Power Energy Wave",
+        "Justice Combination",
+    ],
+}
+const Krillin ={
+    "Normal": [
+        "Full Power Energy Blast Volley",
+        "Kamehameha",
+        "Destructio-Disc",
+        "Chain Destructo-Disc Barrage"
+    ]
+}
+const Tien ={
+    "Normal": [
+        "Full Power Energy Wave",
+        "Tri Beam",
+        "Dodon Ray"
+    ]
+}
+const Yamcha ={
+    "Normal": [
+        "Full Power Energy Blast Volley",
+        "Kamehameha",
+        "Full Power Energy Wave",
+        "Wolf Fang Fist"
+    ]
+}
+function get_random_moves(character, form) {
+    // Randomly select a characters super attack and return character form and super
+    const TFMap = {
+        "Goku": Goku,
+        "Vegeta": Vegeta,
+        "Adult Gohan": ADGohan,
+        "Teen Gohan": TeenGohan,
+        "Goten": Goten,
+        "Gogeta": Gogeta,
+        "Bardock": Bardock,
+        "SBroly": SBroly,
+        "Kid Trunks": KidTrunks,
+        "Adult Trunks":ADTrunks,
+        "Beerus": Beerus,
+        "Whis": Whis,
+        "Piccolo": Piccolo,
+        "Frieza": Frieza,
+        "Cell": Cell,
+        "Buu": Buu,
+        "GT_Pan": GT_Pan,
+        
+    }
+    const transformations = TFMap[character]
+    const moves = transformations[form];
     const move = moves[Math.floor(Math.random() * moves.length)];
-    
-    return { name: character.name, form, move };
-}*/
+    return { name: character, form, move };
+}
 //array of items
 const imagePaths = [
     "Resize/Adult Gohan_Normal_500x500.png",
@@ -62,7 +328,7 @@ const imagePaths = [
 function Roll(){
     console.log('Rolling')
     const boxes = document.querySelectorAll('.boxes')
-    rollInterval = setInterval(() => {
+    rollInterval = setInterval(() => { //Start the rolling mtfs
         boxes.forEach(box => {
           const randomImageIndex = Math.floor(Math.random() * imagePaths.length);
           box.style.backgroundImage = "url('" + imagePaths[randomImageIndex] + "')";
@@ -70,6 +336,18 @@ function Roll(){
     }, 100);
 }
 function Stop() {
+    console.log('stop rolling')
+    clearInterval(rollInterval);
+    const boxes = document.querySelectorAll('.boxes')
+    boxes.forEach((box, index) =>{
+        const ImgUrl = box.style.backgroundImage
+        const imageName = ImgUrl.slice(ImgUrl.lastIndexOf('/') + 1, ImgUrl.lastIndexOf('.'));
+        const [character, form] = imageName.split('_');
+        console.log('Box ' + (index + 1) + ' URL: ' + ImgUrl);
+        console.log(imageName)
+        const data = get_random_moves(character, form)
+        console.log(data)
+    })
 }
 document.getElementById("spin").addEventListener('click', Roll);
 document.getElementById('stop').addEventListener('click', Stop);
